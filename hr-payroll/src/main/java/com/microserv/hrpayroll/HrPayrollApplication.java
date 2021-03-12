6fad99b622c9ad336427d70cd36f1dcd528acbf2
fcojosef <fcojosef.o@gmail.com>
2021-03-11 23:35:13 -0300

    Configuring Eureka Clients

--- hr-payroll/src/main/java/com/microserv/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/microserv/hrpayroll/HrPayrollApplication.java
@@ -2,8 +2,11 @@ package com.microserv.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
